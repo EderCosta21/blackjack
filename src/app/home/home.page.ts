cc13cf52552a85e49a89ce9989654d3a2ae6e414
eder6 <eder@b2mlportal.com.br>
2021-07-30 16:59:14 -0300

    Atualização

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
 import { gerarCartas } from './../services/gerarCartas';
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-home',
@@ -8,14 +10,154 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor( public gerarCarts : gerarCartas) {}
+  public pessoa: any;
+  public maquina: any;
+  public saldoPessoa: any;
+  public saldoMaquina: any;
 
- 
-  ionViewWillEnter(){
+  constructor(public gerarCarts: gerarCartas, public alertController: AlertController) { }
 
-  console.log( "sorteio pessoa",this.gerarCarts.sorteioCartasPessoa())
-  console.log( "sorteio maquina",this.gerarCarts.sorteioCartasMaquina())
+
+  ionViewWillEnter() {
+
+    this.startGame()
   }
+
+  async startGame() {
+    this.pessoa = [this.gerarCarts.sorteioCartas(), this.gerarCarts.sorteioCartas()]
+    this.maquina = [this.gerarCarts.sorteioCartas(), this.gerarCarts.sorteioCartas()]
+
+    await this.countValorPessoa();
+    await this.countValorMaquina();
+
+    console.log("pessoa", this.pessoa)
+    console.log("maquina", this.maquina)
+  }
+
+  countValorPessoa() {
+    let count = 0;
+    this.pessoa.map(res => {
+
+      if (res[0] == 'A') {
+        count += 1
+      }
+      if (res[0] == 'Q') {
+        count = count + 8
+      }
+      if (res[0] == 'J') {
+        count = count + 9
+      }
+      if (res[0] == 'K') {
+        count = count + 10
+      }
+      if (!['A', 'Q', 'J', 'K'].includes(res[0])) {
+        count = count + parseInt(res[0])
+
+      }
+      this.saldoPessoa = count;
+    })
+
+
+    this.verificaValor();
+    console.log("Saldo pessoa", this.saldoPessoa)
+
+  }
+
+  countValorMaquina() {
+    let count = 0;
+    this.maquina.map(res => {
+
+      if (res[0] == 'A') {
+        count += 1
+      }
+      if (res[0] == 'Q') {
+        count = count + 8
+      }
+      if (res[0] == 'J') {
+        count = count + 9
+      }
+      if (res[0] == 'K') {
+        count = count + 10
+      }
+      if (!['A', 'Q', 'J', 'K'].includes(res[0])) {
+        count = count + parseInt(res[0])
+
+      }
+      this.saldoMaquina = count;
+    })
+
+
+    this.verificaValor();
+    console.log("Saldo maquina", this.saldoMaquina)
+
+  }
+
+  async maisUma() {
+    this.pessoa.push(this.gerarCarts.sorteioCartas())
+    console.log("pessoa", this.pessoa)
+
+    await this.countValorPessoa();
+
+
+  }
+
+  async goToMaquina() {
+    let idInterval;
+    if (this.saldoPessoa < 17 || this.saldoPessoa != 21) {
+
+      idInterval = setInterval(() => {
+        this.maquina.push(this.gerarCarts.sorteioCartas());
+        this.countValorMaquina();
+        console.log( "Cartas maquina", this.maquina)
+        if(this.saldoMaquina >=21)
+        {
+          console.log("count", this.saldoMaquina)
+          clearInterval(idInterval);
+        }
+        
+      }, 2000);
+
+    }
+
+  }
+
+
+  verificaValor() {
+    let message;
+    if (this.saldoPessoa < 21) {
+      console.log("Voce continua ")
+    }
+    else if (this.saldoPessoa == 21) {
+      console.log("voce ganhou")
+      message = 'Você ganhou'
+      this.presentAlert(message)
+    }
+    else {
+      console.log("Voce perdeu ")
+      message = 'Você perdeu'
+      this.presentAlert(message)
+
+    }
+  }
+
+
+  async presentAlert(messsage) {
+    const alert = await this.alertController.create({
+      // cssClass: 'my-custom-class',
+      header: 'Atenção',
+      // subHeader: 'Subtitle',
+      message: messsage,
+      buttons: ['OK'],
+      mode: 'ios',
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    this.startGame();
+    console.log('onDidDismiss resolved with role', role);
+  }
+
 
 
 }
--- src/app/services/gerarCartas.ts
+++ src/app/services/gerarCartas.ts
@@ -1,40 +1,31 @@
 export class gerarCartas {
 
+    // public cartas: any = [
+    //     'A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Q', 'J', 'K',
+    //     'A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Q', 'J', 'K',
+    //     'A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Q', 'J', 'K',
+    //     'A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Q', 'J', 'K'
+    // ];
+
     public cartas: any = [
-        'A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Q', 'J', 'K',
-        'A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Q', 'J', 'K',
-        'A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Q', 'J', 'K',
-        'A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Q', 'J', 'K'
+        ['A', '&clubs'], ['2', '&clubs'], ['3', '&clubs'], ['4', '&clubs'], ['5', '&clubs'], ['6', '&clubs'], ['7', '&clubs'], ['8', '&clubs'], ['9', '&clubs'], ['10', '&clubs'], ['Q', '&clubs'], ['J', '&clubs'], ['K', '&clubs'],
+        ['A', '&hearts'], ['2', '&hearts'], ['3', '&hearts'], ['4', '&hearts'], ['5', '&hearts'], ['6', '&hearts'], ['7', '&hearts'], ['8', '&hearts'], ['9', '&hearts'], ['10', '&hearts'], ['Q', '&hearts'], ['J', '&hearts'], ['K', '&hearts'],
+        ['A', '&spades'], ['2', '&spades'], ['3', '&spades'], ['4', '&spades'], ['5', '&spades'], ['6', '&spades'], ['7', '&spades'], ['8', '&spades'], ['9', '&spades'], ['10', '&spades'], ['Q', '&spades'], ['J', '&spades'], ['K', '&spades'],
+        ['A', '&diams'], ['2', '&diams'], ['3', '&diams'], ['4', '&diams'], ['5', '&diams'], ['6', '&diams'], ['7', '&diams'], ['8', '&diams'], ['9', '&diams'], ['10', '&diams'], ['Q', '&diams'], ['J', '&diams'], ['K', '&diams'],
+
+
     ];
-    public naipes: any [
-        
-    ]
 
-    sorteioCartasPessoa() {
-  
+    
+    sorteioCartas() {
+    
         let currentIndex = this.cartas.length
-        let randon1 = Math.floor(Math.random() * currentIndex)+1;
-        let randon2 = Math.floor(Math.random() * currentIndex) + 1;
-       
-        let variavel1 = this.cartas[randon1]
-        let variavel2 = this.cartas[randon2]
-        let v = [variavel1,variavel2]
-   
-        return v 
-    }
-
-    sorteioCartasMaquina() {
-       
-        let currentIndex = this.cartas.length
-        let randon1 = Math.floor(Math.random() * currentIndex) +1 ;
-        let randon2 = Math.floor(Math.random() * currentIndex) + 1;
-       
-        let variavel1 = this.cartas[randon1]
-        let variavel2 = this.cartas[randon2]
-        let v = [variavel1,variavel2]
-   
-        return v 
+        let randon1 = Math.floor(Math.random() * currentIndex);
+        let variavel1 = this.cartas[randon1];
+         this.cartas.splice(randon1,1); 
+        return variavel1
     }
 
   
+
 }
